19e66c3b9fbd26e8dd33f20bac9f8355df7c1239
hema6789 <hema.jampula@gmail.com>
2022-09-09 03:44:03 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,56 +1,60 @@
 import React, { useState } from "react";
-import './App.css';
+import "./App.css";
 import axios from "axios";
 
 const TaxId = () => {
   const [taxId, setTaxId] = useState();
-  const [coundtycode, setcountrycode] = useState();
-  const [cname,setCName] = useState();
-  
+  const [countryCode, setCountryCode] = useState();
+  const [cname, setCName] = useState();
+  const [valid, setValid] = useState();
+  const [address, setAddress] = useState();
+
   const validateTaxID = () => {
     taxId.length > 0 &&
-	coundtycode.length > 0 &&
+      countryCode.length > 0 &&
       axios
         .get(
-          `https://api.taxid.pro/lookup?country=${coundtycode}&tin=${taxId}&key=P4gi4PEaJ3CoYVkCeKCr8RKn1XEJqxEU`,
-          
+          `https://api.taxid.pro/lookup?country=${countryCode}&tin=${taxId}&key=P4gi4PEaJ3CoYVkCeKCr8RKn1XEJqxEU`
         )
-        .then((res) => setCName(res.data.name))
+        .then((res) => {
+          setCName(res.data.name);
+          setAddress(res.data.address);
+          (res.data.valid==true) ? setValid('Valid') : setValid('Not Valid')
+        })
         .catch((err) => console.log(err));
-		//console.log(res);
   };
-   //console.log("SK2023172459");
-  
+  //console.log("SK2023172459");
+
   return (
     <div className="box">
-
       <div className="form-group row">
-          <input
-            className="input"
-            type="text"
-            placeholder="Country Code"
-            onChange={(e) => setcountrycode(e.target.value)}
-          />
-        </div>
-        <div className="form-group row">
-          <input
-            className="input"
-            type="text"
-            placeholder="Vat ID"
-            onChange={(e) => setTaxId(e.target.value)}
-          />
-        </div>
-        <div className="form-group row mt-5">
-          <button className="btn" onClick={() => validateTaxID()}>
-            Validate
-          </button>
+        <select id="lang" onChange={(e) => setCountryCode(e.target.value)}>
+          <option value="de">de</option>
+          <option value="sk">sk</option>
+          <option value="se">se</option>
+        </select>
+      </div>
+      <div className="form-group row">
+        <input
+          className="input"
+          type="text"
+          placeholder="Vat ID"
+          onChange={(e) => setTaxId(e.target.value)}
+        />
+      </div>
+      <div className="form-group row mt-5">
+        <button className="btn" onClick={() => validateTaxID()}>
+          Validate
+        </button>
+
+        <p>
+          Valid : {valid} <br/>
+          Company Name : {cname} <br/>
+          Address : {address}
           
-		  <p>Company Name : {cname}</p>
-      <p> Example -
-        Countrycode = SE  &nbsp;
-        VAT No = SE556656688001
- </p>
-        </div>
+        </p>
+        <p> Example - Countrycode = SE &nbsp; VAT No = SE556656688001</p>
+      </div>
     </div>
   );
 };
